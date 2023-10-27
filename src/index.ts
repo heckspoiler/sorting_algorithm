0fcb7022f3b834042bba8c8b6440f4029d5c1fcf
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-27 23:11:11 +0200

    added linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 2, 3, 4, 5, -9]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 2, 3, 4, 5, -9]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+// const charactersCollection = new CharactersCollection('XaaYbgAef');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(50);
+linkedList.add(20);
+linkedList.add(-3);
+
+const sorter = new Sorter(linkedList);
+
 sorter.sort();
-console.log(numbersCollection.data);
+linkedList.print();
