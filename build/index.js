dace474e5a27d4c237b3aef33971b8293c73b575
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-27 20:26:59 +0200

    sorter algorithm

--- build/index.js
+++ build/index.js
@@ -1,5 +1,21 @@
-"use strict";
-const logSomething = () => {
-    console.log('123');
-};
-logSomething();
+'use strict';
+class Sorter {
+  constructor(collection) {
+    this.collection = collection;
+  }
+  sort() {
+    const { length } = this.collection; // this is the same as const length = this.collection.length
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; (j = length - 1 - i - 1); j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+const sorter = new Sorter([10, 3, 5, -10]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
-const logSomething = () => {
-  console.log('123');
-};
+class Sorter {
+  constructor(public collection: number[]) {}
 
-logSomething();
+  sort(): void {
+    const { length } = this.collection; // this is the same as const length = this.collection.length
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; (j = length - 1 - i - 1); j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter([10, 3, 5, -10]);
+sorter.sort();
+console.log(sorter.collection);
