535bce4073e89027f87beb8a4d9af40ec5828b25
Carlo Ettisberger <carlo_ettisberger@hotmail.com>
2023-10-27 23:23:04 +0200

    issues with inheritance

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -5,15 +5,13 @@ interface Sortable {
 }
 
 export class Sorter {
-  constructor(public collection: Sortable) {}
-
   sort(): void {
-    const { length } = this.collection; // this is the same as const length = this.collection.length
+    const { length } = this; // this is the same as const length = this.collection.length
 
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - 1 - i - 1; j++) {
-        if (this.collection.compare(j, j + 1)) {
-          this.collection.swap(j, j + 1);
+        if (this.compare(j, j + 1)) {
+          this.swap(j, j + 1);
         }
       }
     }
